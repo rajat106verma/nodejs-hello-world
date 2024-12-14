0087ebaed5400d46a264b135990422eadddb5fe4
rajat106verma <112701519+rajat106verma@users.noreply.github.com>
2024-12-14 22:28:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(request, response) {
 
 });
 
-var port = 8085;
+var port = 8086;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
